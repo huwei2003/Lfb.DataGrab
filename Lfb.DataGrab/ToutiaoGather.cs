5b149a8557b6c304dc059fb02c6aa1fbc454349a
unknown <85026424@qq.com>
2016-10-26 18:19:04 +0800

    mod toutiaogather

--- Lfb.DataGrab/ToutiaoGather.cs
+++ Lfb.DataGrab/ToutiaoGather.cs
@@ -67,13 +67,14 @@ namespace Lfb.DataGrab
                 if(isHaveMore)
                 {
                     var max_behot_time = data.next.max_behot_time;
+                    //替换url中的max_behot_time
 
                 }
                 
 
 
                 //判断是否还可以翻页，有则再取数据
-                //http://www.toutiao.com/api/article/feed/?category=__all__&utm_source=toutiao&widen=0&max_behot_time=1477194899&max_behot_time_tmp=1477194899&as=A135F8A09C7897E&cp=580C08C9477EAE1
+                
 
             }
             catch (Exception ex)
@@ -82,6 +83,43 @@ namespace Lfb.DataGrab
             }
             return null;
         }
+
+        /// <summary>
+        /// 处理url,替换其中的时间参数，用以读取下一页数据
+        /// </summary>
+        /// <param name="url">原url</param>
+        /// <param name="newMaxBehotTime">时间</param>
+        /// <returns></returns>
+        public string ModifyUrlMax_behot_time(string url,string newMaxBehotTime)
+        {
+            var reStr = url;
+            try
+            {
+                //http://www.toutiao.com/api/article/feed/?category=__all__&utm_source=toutiao&widen=0&max_behot_time=1477194899&max_behot_time_tmp=1477194899&as=A135F8A09C7897E&cp=580C08C9477EAE1
+
+                var arrStr = url.Split('&');
+                if (arrStr.Length > 0)
+                {
+                    foreach (var item in arrStr)
+                    {
+                        if (item.ToLower().Contains("max_behot_time") && !item.ToLower().Contains("max_behot_time_tmp"))
+                        {
+                            reStr = reStr.Replace(item, "max_behot_time=" + newMaxBehotTime);
+                        }
+                        if (item.ToLower().Contains("max_behot_time_tmp"))
+                        {
+                            reStr = reStr.Replace(item, "max_behot_time_tmp=" + newMaxBehotTime);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+            return reStr;
+        }
+
 
     }
 }
--- Test/ToutiaoGatherTests.cs
+++ Test/ToutiaoGatherTests.cs
@@ -10,10 +10,29 @@ namespace Lfb.DataGrab.Tests
     [TestFixture()]
     public class ToutiaoGatherTests
     {
+        private ToutiaoGather bll;
+        /// <summary>
+        /// 全局setup,不能使用async OneTimeSetUpAttribute  or TestFixtureSetUp
+        /// </summary>
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+
+        }
+
+        /// <summary>
+        /// 每次测试setup,不能使用async
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            bll = new ToutiaoGather();
+
+        }
         [Test()]
         public void NewsUrlGatheringTest()
         {
-            ToutiaoGather bll = new ToutiaoGather();
+            
             var url = "http://www.toutiao.com/api/article/feed/?category=__all__&utm_source=toutiao&widen=0&max_behot_time=0&max_behot_time_tmp=0&as=A1D5B8000CC7350&cp=580C772325900E1";
             bll.NewsUrlGathering(url, 100);
         }
@@ -34,6 +53,15 @@ namespace Lfb.DataGrab.Tests
         public void NewsPicGatheringOneTest()
         {
             Assert.Fail();
+        }
+
+        [Test()]
+        public void ModifyUrlMax_behot_timeTest()
+        {
+            var url =
+                "http://www.toutiao.com/api/article/feed/?category=__all__&utm_source=toutiao&widen=0&max_behot_time=1477194899&max_behot_time_tmp=1477194899&as=A135F8A09C7897E&cp=580C08C9477EAE1";
+            var result = bll.ModifyUrlMax_behot_time(url, "1481111222");
+            Assert.IsTrue(result.Contains("1481111222"));
         }
     }
 }
