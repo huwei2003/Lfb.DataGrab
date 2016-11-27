299bfead33955a933a3eb0462d93c33b3ac7e4e3
huwei2003 <85026424@qq.com>
2016-11-27 21:27:36 +0800

    mod dto

--- Lfb.DataGrabBll/Dto/DtoTouTiaoAuthorNews.cs
+++ Lfb.DataGrabBll/Dto/DtoTouTiaoAuthorNews.cs
@@ -6,23 +6,23 @@ namespace Lfb.DataGrabBll.Dto
     public class DtoTouTiaoAuthorNews
     {
         //public int play_effective_count;
-        public int gallery_pic_count;
+        //public int gallery_pic_count;
         //阅读量
         public string go_detail_count;
         public string title;
-        public List<DtoTouTiaoImageList> image_list;
+        //public List<DtoTouTiaoImageList> image_list;
         //public string abstract;
         //public int show_play_effective_count;
-        public bool middle_mode;
-        public bool has_video;
+        //public bool middle_mode;
+        //public bool has_video;
         public string source_url;
         public DateTime datetime;
         public string live_status;
         public bool more_mode;
         public string comments_count;
-        public string video_duration_str;
-        public bool has_gallery;
-        public string pc_image_url;
+        //public string video_duration_str;
+        //public bool has_gallery;
+        //public string pc_image_url;
         
     }
 }
